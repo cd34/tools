47b00c23ff89933b258883eec3d422dfd5841ef3
Chris Davies <mcd@daviesinc.com>
2013-06-16 17:09:09 -0400

    fix bash argv

--- kvm/ded2virtgrub.sh
+++ kvm/ded2virtgrub.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -x
 
-DEVICE=$0
-LV=$1
-PORT=$2
+DEVICE=$1
+LV=$2
+PORT=$3
 
 virt-install --force -c grub2-boot.iso -n $DEVICE -r 1536 --vcpus=1 --disk /dev/vg0/$LV,bus=scsi --vnc --vnclisten=0.0.0.0  --vncport=$PORT --noautoconsole --os-type linux --os-variant debiansqueeze --accelerate --network=bridge:br0,model=virtio --hvm
